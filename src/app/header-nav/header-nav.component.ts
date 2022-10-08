094a35bde21ecc57c516a9d51e9a2620467ecaa9
Reagan Henke <reaganhenke@gmail.com>
2022-10-07 22:52:24 -0400

    mobile menu

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header-nav.component.html',
   styleUrls: ['./header-nav.component.scss']
 })
-export class HeaderNavComponent implements OnInit {
+export class HeaderNavComponent {
+  showMobileMenu = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  navigate() {
+    this.toggleMobileMenu(false);
   }
 
+  toggleMobileMenu(state = !this.showMobileMenu) {
+    if (state) {
+      document.getElementById("path1")?.setAttribute("d", "M10,10 L30,30");
+      document.getElementById("path2")?.setAttribute("d", "M10,10 L30,30");
+      document.getElementById("path3")?.setAttribute("d", "M10,30 L30,10");
+    } else {
+      document.getElementById("path1")?.setAttribute("d", "M10,10 L30,10");
+      document.getElementById("path2")?.setAttribute("d", "M10,20 L30,20");
+      document.getElementById("path3")?.setAttribute("d", "M10,30 L30,30");
+    }
+    this.showMobileMenu = state;
+  }
 }
