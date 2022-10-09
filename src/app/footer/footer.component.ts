0f330f6011070da4d796d397262da17d2738bfcd
Reagan Henke <reaganhenke@gmail.com>
2022-10-09 15:51:32 -0400

    home page for desktop and mobile

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Event, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  onHome = true;
 
-  constructor() { }
+  constructor(private router: Router) {
 
-  ngOnInit(): void {
+    router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationStart) {
+        this.onHome = event.url === "/"
+      }
+    })
   }
-
 }
