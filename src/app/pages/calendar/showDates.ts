a71236f8e7c30e315c9bd5b1d2cc64fcc2175255
Reagan Henke <reaganhenke@gmail.com>
2022-10-18 18:38:10 -0400

    shared button styles

--- src/app/pages/calendar/showDates.ts
+++ src/app/pages/calendar/showDates.ts
@@ -1,5 +1,25 @@
 interface Show {
-  date: String
+  date: Date;
+  time: string;
+  location: string;
+  locationLink?: string;
+  ticketLink?: string;
+  cast?: {
+    frank: string;
+    janet: string;
+    brad: string;
+    riff: string;
+    magenta: string;
+    columbia: string;
+    rocky: string;
+    eddie: string;
+    scott: string;
+    crim: string;
+    trixie: string;
+    lights: string;
+    stagemanager: string;
+    runningcrew: string;
+  }
 };
 
 interface Dates {
@@ -8,6 +28,26 @@ interface Dates {
 
 export const showDates: Dates = {
   "1014": {
-    date: "October 14th"
+    date: new Date('October 14, 2022'),
+    time: "11:55pm",
+    location: "Landmark's E Street Cinema",
+    locationLink: "https://www.landmarktheatres.com/our-locations/x06oa-landmark-e-street-cinema/",
+    ticketLink: "https://tickets.fandango.com/MobileExpress/Checkout?row_count=378081735&mid=146&chainCode=L&sdate=2022-10-28%2023:55&tid=AAREF",
+    cast: {
+      frank: "Ali",
+      janet: "Reagan",
+      brad: "Kat",
+      riff: "Mary",
+      magenta: "Kelly H",
+      columbia: "Donna",
+      rocky: "Damia",
+      eddie: "Mike 1.0",
+      scott: "Caitlyn",
+      crim: "n/a",
+      trixie: "Donna",
+      lights: "Lee",
+      stagemanager: "Relle",
+      runningcrew: "Mike 1.0, Caitlyn, Brad, Grace, Conall, Spencer, Nat, Zoe, Emy, Henry, Lilka, Bryan, Haik"
+    }
   }
 }
