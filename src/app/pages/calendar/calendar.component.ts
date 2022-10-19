89bf6354d1c9a55813499a6dcfb7271d58ed00e2
Reagan Henke <reaganhenke@gmail.com>
2022-10-19 17:51:05 -0400

    desktop and mobile pages for calendar

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { showDates } from './showDates';
+import { showDates, Week, Show } from './showDates';
 
 @Component({
   selector: 'app-calendar',
@@ -10,6 +10,10 @@ import { showDates } from './showDates';
 export class CalendarComponent implements OnInit {
   date: string = "";
   showDates = showDates;
+  currentMonth = new Date();
+  months = ["January","February","March","April","May","June","July",
+  "August","September","October","November","December"];
+  calendarData: Week[] = [];
 
   constructor(private route: ActivatedRoute) {}
 
@@ -17,14 +21,39 @@ export class CalendarComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       this.date = params['date'];
     });
+    this.populateCalendar();
   }
 
-  getFormattedDate(date: Date) {
+  isShowUpcoming(show: Show) {
+    const today = new Date();
+    return show.date > new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+  }
+
+  getFormattedMonth() {
+    const month = this.currentMonth.toLocaleDateString('en-us', { month:"long"});
+    const year = this.currentMonth.getFullYear();
+    return `${month} ${year}`;
+  }
+
+  incrementMonth(delta: number) {
+    const newMonth = (this.currentMonth.getMonth() + delta);
+    const newYear = this.currentMonth.getFullYear();
+    this.currentMonth = new Date(newYear, newMonth, 1)
+    this.populateCalendar();
+  }
+
+  getShowsInCurrentMonth() {
+    return Object.entries(showDates).filter(([id, show]) => {
+      return (show.date.getMonth() === this.currentMonth.getMonth() && show.date.getFullYear() === this.currentMonth.getFullYear());
+    });
+  }
+
+  getFormattedDate(date: Date, includeYear = true) {
     const day = date.toLocaleDateString('en-us', { weekday:"long"});
     const month = date.toLocaleDateString('en-us', { month:"long"});
     const dayNum = date.getDate();
     const year = date.getFullYear();
-    return `${day} ${month} ${this.getOrdinalNum(dayNum)}, ${year}`
+    return includeYear ? `${day} ${month} ${this.getOrdinalNum(dayNum)}, ${year}` : `${day} ${month} ${this.getOrdinalNum(dayNum)}`;
   }
 
   getOrdinalNum(number: number):string {
@@ -40,4 +69,24 @@ export class CalendarComponent implements OnInit {
   
     return number + ['th', 'st', 'nd', 'rd', ''][selector];
   };
+
+  populateCalendar() {
+    this.calendarData = [];
+    const showsThisMonth = this.getShowsInCurrentMonth();
+    const numberOfDaysInMonth = new Date(this.currentMonth.getFullYear(),this.currentMonth.getMonth() + 1, 0).getDate();
+    const firstDayOfWeek = new Date(this.currentMonth.getFullYear(),this.currentMonth.getMonth(), 1).getDay();
+    let day = 0;
+    for (let weekIndex = 0; weekIndex < Math.ceil((firstDayOfWeek + numberOfDaysInMonth)/7); weekIndex++) {
+      let week = [];
+      for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
+        const calculatedDay = (day < firstDayOfWeek || day >= firstDayOfWeek + numberOfDaysInMonth) ? 0 : day - firstDayOfWeek + 1;
+        const shows = showsThisMonth.filter(([id, show]) => {
+          return (show.date.getDate() === calculatedDay);
+        })
+        week.push([calculatedDay, shows])
+        day++;
+      }
+      this.calendarData.push(week as Week);
+    }
+  }
 }
--- src/app/pages/calendar/showDates.ts
+++ src/app/pages/calendar/showDates.ts
@@ -1,4 +1,4 @@
-interface Show {
+export interface Show {
   date: Date;
   time: string;
   location: string;
@@ -22,9 +22,13 @@ interface Show {
   }
 };
 
-interface Dates {
+export interface Dates {
   [key: string]: Show;
 }
+
+export type Day = [number, [string, Show][]] | [number, []];
+
+export type Week = [Day, Day, Day, Day, Day, Day, Day];
 
 export const showDates: Dates = {
   "1014": {
@@ -49,5 +53,46 @@ export const showDates: Dates = {
       stagemanager: "Relle",
       runningcrew: "Mike 1.0, Caitlyn, Brad, Grace, Conall, Spencer, Nat, Zoe, Emy, Henry, Lilka, Bryan, Haik"
     }
+  },
+  "1015": {
+    date: new Date('October 15, 2022'),
+    time: "11:55pm",
+    location: "Landmark's E Street Cinema",
+    locationLink: "https://www.landmarktheatres.com/our-locations/x06oa-landmark-e-street-cinema/",
+    ticketLink: "https://tickets.fandango.com/MobileExpress/Checkout?row_count=378081735&mid=146&chainCode=L&sdate=2022-10-28%2023:55&tid=AAREF",
+  },
+  "1115": {
+    date: new Date('November 15, 2022'),
+    time: "11:55pm",
+    location: "Landmark's E Street Cinema"
+  },
+  "10131": {
+    date: new Date('October 13, 2022'),
+    time: "11:55pm",
+    location: "Landmark's E Street Cinema"
+  },
+  "10132": {
+    date: new Date('October 13, 2022'),
+    time: "5:55pm",
+    location: "Landmark's E Street Cinema"
+  },
+  "123": {
+    date: new Date('October 19, 2022'),
+    time: "5:55pm",
+    location: "Landmark's E Street Cinema",
+    ticketLink: "example.com"
+  },
+  "1234": {
+    date: new Date('October 18, 2022'),
+    time: "5:55pm",
+    location: "Landmark's E Street Cinema",
+    ticketLink: "example.com"
+  },
+  "12345": {
+    date: new Date('October 20, 2022'),
+    time: "5:55pm",
+    location: "Landmark's E Street Cinema",
+    ticketLink: "example.com"
+
   }
 }
