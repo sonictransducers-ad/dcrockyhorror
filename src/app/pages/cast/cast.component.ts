c85d3df4f2abde8542d5a6b410ec9a1d45475cdd
Reagan Henke <reaganhenke@gmail.com>
2023-02-23 16:32:15 -0500

    move pronouns, reorganize members, add mike, oscar, and spencer

--- src/app/pages/cast/cast.component.ts
+++ src/app/pages/cast/cast.component.ts
@@ -9,15 +9,6 @@ import { Member } from './cast.interface';
 export class CastComponent {
   castList: Member[] = [
     {
-      name: "Leandra",
-      pronouns: "",
-      photoUrl: "assets/cast/leandra.png",
-      title: "cast lead",
-      yearJoined: "2009",
-      roles: "Magenta, Brad, Frank, Riff Raff, Eddie, Dr. Scott, Columbia, Transylvanian, Trixie, MC, Criminologist, and Janet",
-      whyJoin: "I don't know... I've been doing it for a million years and I'm sure I had a great reason at the time. It was probably some teenage rebellion thing. I keep doing it for the money, the fame, and all the fringe benefits of dressing in my underwear in front of strangers."
-    },
-    {
       name: "Kelly",
       pronouns: "",
       photoUrl: "/assets/cast/kelly.jpg",
@@ -25,6 +16,15 @@ export class CastComponent {
       yearJoined: "Sep 2017",
       roles: "Trixie, Magenta, and Janet",
       whyJoin: "I love this movie and I love attention!"
+    },
+    {
+      name: "Leandra",
+      pronouns: "",
+      photoUrl: "assets/cast/leandra.png",
+      title: "cast lead",
+      yearJoined: "2009",
+      roles: "Magenta, Brad, Frank, Riff Raff, Eddie, Dr. Scott, Columbia, Transylvanian, Trixie, MC, Criminologist, and Janet",
+      whyJoin: "I don't know... I've been doing it for a million years and I'm sure I had a great reason at the time. It was probably some teenage rebellion thing. I keep doing it for the money, the fame, and all the fringe benefits of dressing in my underwear in front of strangers."
     },
     {
       name: "Ali",
@@ -52,14 +52,6 @@ export class CastComponent {
       whyJoin: "Because waving a fiery torch and fronting a fire circus wasn't enough of a theatrical challenge."
     },
     {
-      name: "Lindsay",
-      pronouns: "",
-      photoUrl: "assets/cast/placeholder1.png",
-      yearJoined: "June 2017",
-      roles: "Magenta",
-      whyJoin: "To meet like minded sick fucks!"
-    },
-    {
       name: "Relle",
       pronouns: "",
       photoUrl: "assets/cast/placeholder5.png",
@@ -68,9 +60,18 @@ export class CastComponent {
       whyJoin: "I do what Leandra tells me to do; it's safer that way. The only thing scarier than what happens if I catch you touching the props is Leandra's epic stink eye."
     },   
     {
+      name: "Lindsay",
+      pronouns: "",
+      photoUrl: "assets/cast/placeholder1.png",
+      yearJoined: "June 2017",
+      roles: "Magenta",
+      whyJoin: "To meet like minded sick fucks!"
+    },
+    {
       name: "Raygun",
       pronouns: "she/her",
       photoUrl: "assets/cast/raygun.png",
+      title: "PR Director",
       yearJoined: "Dec 2017",
       roles: "Janet",
       aboutMe: "I've never lost a sock in the laundry.",
@@ -86,14 +87,6 @@ export class CastComponent {
       favCallback: "Show us how a butterfly masturbates!"
     },
     {
-      name: "Lady Morgan",
-      pronouns: "she/any",
-      photoUrl: "assets/cast/ladym.jpg",
-      yearJoined: "Oct 2021",
-      roles: "Columbia & Trixie...for now",
-      aboutMe: "I realized from the first time I saw Frank standing on the globe singing 'Don't dream it...be it,' that this show was going to change my life. This philosophy has led me to perform over 400 times, before thousands of people. It's fueled decisions that have led me to have some amazing adventures! It's also picked me up from some hard tragedies as well. The RHPS family is Welcoming, fierce, and home to some of the most amazing people I've ever met. ",
-    },
-    {
       name: "Caitlyn",
       pronouns: "she/her",
       photoUrl: "assets/cast/caitlyn.jpg",
@@ -103,12 +96,46 @@ export class CastComponent {
       aboutMe: "Weird and Fun Loving. Love to meet new people and do activities with them. You're now my new best friend."
     },
     {
+      name: "Lady Morgan",
+      pronouns: "she/any",
+      photoUrl: "assets/cast/ladym.jpg",
+      yearJoined: "Oct 2021",
+      roles: "Columbia & Trixie...for now",
+      aboutMe: "I realized from the first time I saw Frank standing on the globe singing 'Don't dream it...be it,' that this show was going to change my life. This philosophy has led me to perform over 400 times, before thousands of people. It's fueled decisions that have led me to have some amazing adventures! It's also picked me up from some hard tragedies as well. The RHPS family is Welcoming, fierce, and home to some of the most amazing people I've ever met. ",
+    },
+    {
       name: "Lilka",
       pronouns: "she/her",
       photoUrl: "assets/cast/lilka.jpg",
       yearJoined: "Sep 2022",
       roles: "Eddie, Crim, Dr. Scott, Translyvanian",
       favCallback: "Hey Frank, say something in Klingon!"
+    },
+    {
+      name: "Oscar",
+      pronouns: "he/him",
+      photoUrl: "assets/cast/oscar.png",
+      title: "Cast Consultant",
+      yearJoined: "Aug 2010",
+      roles: "Brad, Rocky, Eddie, Dr. Scott, Trixie, Ralph, Transylvanian, and Tech Crew",
+      favCallback: `It's such a silly one, when Frank says “seal of approval”, and then you clap and bark like an actual seal. So dumb, but it tickles me every time.`,
+      instagram: "https://www.instagram.com/CROCODLLE/"
+    },
+    {
+      name: "Mike",
+      pronouns: "any pronouns",
+      photoUrl: "assets/cast/mike2.jpg",
+      yearJoined: "Feb 2022",
+      roles: "Riff, Brad, Eddie, and Crim",
+      favCallback: "What's the worst city in Maryland? Columbia."
+    },
+    {
+      name: "Spencer",
+      pronouns: "he/him",
+      photoUrl: "assets/cast/spencer.jpg",
+      yearJoined: "Sep 2022",
+      roles: "Trixie and Rocky",
+      whyJoin: "Because I'm an attention whore!!",
     },
     {
       name: "Jake",
