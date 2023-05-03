dfbea7c8123390be1b48b6576ab28b3994f992dc
Reagan Henke <reaganhenke@gmail.com>
2023-05-03 11:54:31 -0400

    add hedwig page

---
+++ src/app/pages/hedwig-page/hedwig-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hedwig-page',
+  templateUrl: './hedwig-page.component.html',
+  styleUrls: ['./hedwig-page.component.scss']
+})
+export class HedwigPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
