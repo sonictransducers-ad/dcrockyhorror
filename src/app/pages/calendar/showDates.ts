a03f9d01f0c7e96f79acc08fa2f1246aec8085dd
Reagan Henke <reaganhenke@gmail.com>
2026-09-19 21:39:42 -0400

    fix ticket links

--- src/app/pages/calendar/showDates.ts
+++ src/app/pages/calendar/showDates.ts
@@ -1007,7 +1007,7 @@ export const showDates: Dates = {
     location: 'Landmark Bethesda Row Cinema',
     locationLink:
       'https://www.landmarktheatres.com/our-locations/x06c1-landmark-bethesda-row-cinema/',
-    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/f39c32ee-e812-5c45-bef0-cef0597df380'
+    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/97a3364a-3bfb-5634-a939-5f6a7bd15112'
   },
   '101026': {
     date: new Date('October 10, 2026'),
@@ -1015,7 +1015,7 @@ export const showDates: Dates = {
     location: 'Landmark Bethesda Row Cinema',
     locationLink:
       'https://www.landmarktheatres.com/our-locations/x06c1-landmark-bethesda-row-cinema/',
-    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/1c0cd5a6-540c-5000-a392-551bde172885'
+    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/5d957df6-e31f-5eaa-96cd-96ec0fde37fd'
   },
   '102326': {
     date: new Date('October 23, 2026'),
@@ -1023,7 +1023,7 @@ export const showDates: Dates = {
     location: 'Landmark Bethesda Row Cinema',
     locationLink:
       'https://www.landmarktheatres.com/our-locations/x06c1-landmark-bethesda-row-cinema/',
-    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/5c2681fb-d7f9-5439-8d1c-355237b1fe95'
+    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/f527f78b-a8c4-5827-8677-10918d3ed959'
   },
   '102426': {
     date: new Date('October 24, 2026'),
@@ -1031,7 +1031,7 @@ export const showDates: Dates = {
     location: 'Landmark Bethesda Row Cinema',
     locationLink:
       'https://www.landmarktheatres.com/our-locations/x06c1-landmark-bethesda-row-cinema/',
-    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/704a56a2-9e79-556b-8dfa-dc005db6ab47'
+    ticketLink: 'https://booking.landmarktheatres.com/launch/ticketing/033679b2-cad9-504a-ab0f-5b9641109279'
   },
   '103126': {
     date: new Date('October 31, 2026'),
